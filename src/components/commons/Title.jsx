4906fe9ac68e234c09de7c7bdb4597f1813b0530
Mert-Ozbolat <mertozbolat008@gmail.com>
2024-12-26 17:06:41 +0300

    updated

--- src/components/commons/Title.jsx
+++ src/components/commons/Title.jsx
@@ -2,36 +2,50 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { color } from '../../constants/color'
 
+
 export const titleTypes = {
-    TEXT_12_400_18: '  TEXT_12_400_18',
+    TEXT_14_400: 'TEXT_14_400',
+    TEXT_14_700: 'TEXT_14_700',
     TEXT_16_700: 'TEXT_16_700',
+    TEXT_12_400_18: 'TEXT_12_400_18',
     TEXT_22_700_40: 'TEXT_22_700_40',
 };
 
 const Title = ({ theme = 'default', text }) => {
-    return (
-        <Text style={styles[theme]}>{text}</Text>
-    )
-}
+    return <Text style={styles[theme]}>{text}</Text>;
+};
 
-export default Title
+export default Title;
 
 const styles = {
     TEXT_12_400_18: StyleSheet.create({
-        color: color.WHITE,
+        color: '#fff',
         fontSize: 12,
         lineHeight: 18,
         fontWeight: '400',
     }),
+
+    TEXT_14_400: StyleSheet.create({
+        color: '#000',
+        fontSize: 14,
+        fontWeight: '700',
+    }),
+    TEXT_14_700: StyleSheet.create({
+        color: '#000',
+        fontSize: 14,
+        fontWeight: '700',
+    }),
+
     TEXT_16_700: StyleSheet.create({
         color: '#000',
         fontSize: 16,
         fontWeight: '700',
     }),
+
     TEXT_22_700_40: StyleSheet.create({
         color: '#000',
         fontSize: 22,
         fontWeight: '700',
-        lineHeight: 40
+        lineHeight: 40,
     }),
-}
+};
--- src/screens/CommonScreens/ProfileScreen.jsx
+++ src/screens/CommonScreens/ProfileScreen.jsx
@@ -1,23 +1,80 @@
 import { StyleSheet, Text, View } from 'react-native'
 import Header from '../../components/commons/Header'
 import Avatar from '../../components/Avatar/Avatar'
+import ProfileData from '../../components/ProfileFlow/ProfileData'
 
 
 
-const source = "https://i.pinimg.com/1200x/8d/65/3b/8d653b3ec2fa7d47ba5f0cae9b71107a.jpg"
+const source = "https://i.pinimg.com/1200x/f2/de/46/f2de4688f716ea3696bbec29441b1751.jpg"
 
 const ProfileScreen = () => {
 
     return (
         <View>
             <Header screenName={'ProfileScreen'} isMyProfile={true} />
-            <View>
+            <View style={styles.userProfileData}>
                 <Avatar image={source} />
+
+                <View style={styles.data}>
+                    <ProfileData value={'1,234'} title={"Post"} />
+                    <ProfileData value={'5,234'} title={"Followers"} />
+                    <ProfileData value={'9,234'} title={"Following"} />
+                </View>
+
             </View>
+
+            <View style={styles.userNameAndInfo}>
+                <Text style={styles.userName}>Username</Text>
+                <Text style={styles.category}>Category/Genre text</Text>
+
+                <Text style={styles.describtion}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, quae.</Text>
+                <Text style={styles.link}>Link goes here</Text>
+            </View>
+
         </View>
     )
 }
 
 export default ProfileScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    userProfileData: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 12,
+        alignItems: 'center'
+    },
+
+    data: {
+        flexDirection: 'row',
+        gap: 24,
+        color: '#000'
+    },
+    userNameAndInfo: {
+        padding: 6,
+        paddingHorizontal: 12,
+        paddingBottom: 10
+    },
+    userName: {
+        color: '#000',
+        fontSize: 13,
+        fontWeight: '700'
+    },
+    category: {
+        fontSize: 13,
+        fontWeight: '400',
+        color: '#000'
+    },
+    describtion: {
+        fontSize: 13,
+        fontWeight: '400',
+        lineHeight: 16,
+        color: '#000'
+    },
+    link: {
+        color: '#004c8b',
+        fontWeight: '700',
+        fontSize: 13,
+        lineHeight: 16
+    },
+})
