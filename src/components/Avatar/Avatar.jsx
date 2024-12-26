34d685dfb6d1d5ca2ac78a537498ccf481b5baf2
Mert-Ozbolat <mertozbolat008@gmail.com>
2024-12-26 12:47:13 +0300

    updated

---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+import { StoryRing } from './StoryRing'
+
+
+const Avatar = ({ image }) => {
+
+
+    return (
+        <View style={styles.avatar}>
+            <StoryRing />
+            <Image style={styles.image} source={{ uri: image }} />
+        </View>
+    )
+}
+
+export default Avatar
+
+const styles = StyleSheet.create({
+    avatar: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 75,
+        height: 75,
+        borderRadius: 999,
+        position: 'absolute'
+    },
+})
--- src/screens/CommonScreens/ProfileScreen.jsx
+++ src/screens/CommonScreens/ProfileScreen.jsx
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import Header from '../../components/commons/Header'
+import Avatar from '../../components/Avatar/Avatar'
 
+
+
+const source = "https://i.pinimg.com/1200x/8d/65/3b/8d653b3ec2fa7d47ba5f0cae9b71107a.jpg"
 
 const ProfileScreen = () => {
+
     return (
         <View>
             <Header screenName={'ProfileScreen'} isMyProfile={true} />
-            <Text>ProfileScreen</Text>
+            <View>
+                <Avatar image={source} />
+            </View>
         </View>
     )
 }
